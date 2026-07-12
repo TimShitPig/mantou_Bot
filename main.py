85e2ef99cdee966660a2d6497205926f0a5347e9
TimShitPig <17818716310@163.com>
2026-07-12 17:49:04 +0800

    更新版本号至 v2.12.5，添加番茄分发诊断日志

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import importlib
 import json
 import sys
 
+from astrbot.api import logger
 from astrbot.api.event import AstrMessageEvent, filter
 from astrbot.api.star import Context, Star, register
 
@@ -48,7 +49,7 @@ Y6API番茄小说模块 = 加载功能模块("功能文件.API功能.Y6api.番�
 获取随机一言回复 = getattr(随机一言模块, "获取随机一言回复")
 获取随机英文单词回复 = getattr(随机英文单词模块, "获取随机英文单词回复")
 获取命令文本 = getattr(消息工具, "获取命令文本")
-插件版本 = "2.12.4"
+插件版本 = "2.12.5"
 
 
 @register("馒头bot", "馒头", "适用于 AstrBot 的馒头bot插件。", 插件版本)
@@ -239,10 +240,19 @@ class MyPlugin(Star):
                     event.stop_event()
                     return
 
-                if 番茄小说模块.番茄小说API是否开启(self.config):
+                番茄API已开启 = 番茄小说模块.番茄小说API是否开启(self.config)
+                if 番茄API已开启:
                     番茄回复流 = 番茄小说模块.获取番茄小说回复流(event, 命令文本, self.config)
+                    if 番茄回复流 is not None:
+                        logger.info("番茄小说分发：番茄API已开启，使用番茄API链路")
+                    else:
+                        本地番茄可识别 = 番茄小说功能.获取番茄小说回复流(event, 命令文本, self.config) is not None
+                        if 本地番茄可识别:
+                            logger.warning("番茄小说分发异常：番茄API已开启，但API链路未识别该番茄链接；为避免绕过开关，未回退本地链路")
                 else:
                     番茄回复流 = 番茄小说功能.获取番茄小说回复流(event, 命令文本, self.config)
+                    if 番茄回复流 is not None:
+                        logger.info("番茄小说分发：番茄API未开启，使用本地番茄链路")
                 if 番茄回复流 is not None:
                     if not 小说功能开关.小说功能是否开启("番茄", self.config):
                         yield event.plain_result(小说功能开关.获取小说功能关闭回复("番茄"))
